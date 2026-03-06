1d7bbf46a20d83fd1772c126c141adfb3f57b109
Jayden Andrews <jaydenrandrews@gmail.com>
2026-03-06 15:19:56 -0500

    Implemented Person Class

--- src/main/java/org/codedifferently/Person.java
+++ src/main/java/org/codedifferently/Person.java
@@ -5,4 +5,34 @@ public class Person {
     private String name;
     private String email;
     private ArrayList<Workout> workouts;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.workouts = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Workout> getWorkouts() {
+        return workouts;
+    }
+
+    public void setWorkouts(ArrayList<Workout> workouts) {
+        this.workouts = workouts;
+    }
 }
