27aa312a32b8a4942133f49b2536bdf2d4c00a42
Bryant Ferguson <btbf569708@gmail.com>
2026-03-06 17:01:50 -0500

    Added helper methods for displaying the leaderboard

--- src/main/java/org/codedifferently/FitnessApp.java
+++ src/main/java/org/codedifferently/FitnessApp.java
@@ -1,8 +1,69 @@
 package org.codedifferently;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class FitnessApp {
     public static void startApp(Scanner sc) {
 
+    }
+
+    public static Person getMaxKey(Map<Person, Integer> map) {
+        int maxExercises = -1;
+        Person maxPerson = null;
+
+        for (Person person : map.keySet()) {
+            if (map.get(person) > maxExercises) {
+                maxExercises = map.get(person);
+                maxPerson = person;
+            }
+        }
+
+        return maxPerson;
+    }
+
+    public static ArrayList<Person> buildLeaderboard(Map<Person, Integer> map) {
+        ArrayList<Person> leaderboard = new ArrayList<>();
+
+        while (!map.isEmpty()) {
+            Person maxPerson = getMaxKey(map);
+            leaderboard.add(maxPerson);
+            map.remove(maxPerson);
+        }
+
+        return leaderboard;
+    }
+
+    public static int countExercises(Person person) {
+        int count = 0;
+
+        for (Workout workout : person.getWorkouts()) {
+            count += workout.getExercises().size();
+        }
+
+        return count;
+    }
+
+    public void displayLeaderboard(ArrayList<Person> people) {
+
+        Map<Person, Integer> map = new LinkedHashMap<>();
+
+        for (Person person : people) {
+            int totalExercises = countExercises(person);
+            map.put(person, totalExercises);
+        }
+
+        Map<Person, Integer> copy = new LinkedHashMap<>(map);
+
+        ArrayList<Person> leaderboard = buildLeaderboard(copy);
+
+        int rank = 1;
+
+        for (Person person : leaderboard) {
+            System.out.println(rank + ". " + person.getName() +
+                    " completed " + map.get(person) + " exercises");
+            rank++;
+        }
     }
 }
--- src/main/java/org/codedifferently/Workout.java
+++ src/main/java/org/codedifferently/Workout.java
@@ -3,61 +3,114 @@ package org.codedifferently;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * The Workout class represents a workout session.
+ * It stores information about the workout duration, date,
+ * completion status, and the exercises performed.
+ */
 public class Workout {
+
+    // Length of the workout (ex: "45 minutes")
     private String duration;
+
+    // Date the workout occurred
     private Date date;
+
+    // Indicates whether the workout has been completed
     private boolean completed;
+
+    // List of exercises included in the workout
     private ArrayList<Exercise> exercises;
 
-    public Workout(String duration, Date date, boolean completed, ArrayList<Exercise> exercises) {
+    public Workout(String duration, Date date, boolean completed) {
         this.duration = duration;
         this.date = date;
         this.completed = completed;
+
+        // Initialize the exercises list
         this.exercises = new ArrayList<>();
     }
 
+    /**
+     * Gets the workout duration
+     * @return duration of the workout
+     */
     public String getDuration() {
         return duration;
     }
 
+    /**
+     * Sets the workout duration
+     * @param duration new workout duration
+     */
     public void setDuration(String duration) {
         this.duration = duration;
     }
 
+    /**
+     * Gets the date of the workout
+     * @return workout date
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Sets the date of the workout
+     * @param date new workout date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * Checks if the workout is completed
+     * @return true if completed, false otherwise
+     */
     public boolean isCompleted() {
         return completed;
     }
 
+    /**
+     * Sets the workout completion status
+     * @param completed completion status
+     */
     public void setCompleted(boolean completed) {
         this.completed = completed;
     }
 
+    /**
+     * Gets the list of exercises in the workout
+     * @return list of exercises
+     */
     public ArrayList<Exercise> getExercises() {
         return exercises;
     }
 
+    /**
+     * Adds a new exercise to the workout
+     * @param exercise exercise to be added
+     */
     public void addExercise(Exercise exercise){
         exercises.add(exercise);
     }
 
+    /**
+     * Displays detailed information about the workout
+     * including its exercises.
+     */
     public void displayWorkoutInfo(){
-            System.out.println("******************************");
-            System.out.println("\nWorkout Date " + date);
-            System.out.println("Duration: " + duration);
-            System.out.println("Status: " + completed);
+        System.out.println("******************************");
+        System.out.println("\nWorkout Date: " + date);
+        System.out.println("Duration: " + duration);
+        System.out.println("Status: " + completed);
 
-            for (Exercise exercise : exercises) {
-                System.out.println(exercise);
+        // Loop through each exercise and print it
+        for (Exercise exercise : exercises) {
+            System.out.println(exercise);
         }
-            System.out.println("******************************");
+
+        System.out.println("******************************");
     }
 
 }
