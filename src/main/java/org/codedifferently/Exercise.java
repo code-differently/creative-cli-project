62ea84d9275b9ecf55c03df4a21749f83531a2a4
Bryant Ferguson <btbf569708@gmail.com>
2026-03-06 15:34:38 -0500

    Feat: created workout class

--- src/main/java/org/codedifferently/Exercise.java
+++ src/main/java/org/codedifferently/Exercise.java
@@ -1,4 +1,37 @@
 package org.codedifferently;
 
 public class Exercise {
+    private WorkoutType type;
+    private String name;
+    private int caloriesBurned;
+
+    public Exercise(WorkoutType type, String name, int caloriesBurned) {
+        this.type = type;
+        this.name = name;
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    public WorkoutType getType() {
+        return type;
+    }
+
+    public void setType(WorkoutType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    public void setCaloriesBurned(int caloriesBurned) {
+        this.caloriesBurned = caloriesBurned;
+    }
 }
